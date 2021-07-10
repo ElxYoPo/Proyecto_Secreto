00631bd9a92b2d6b200f825f72b5531f920dd5a0
Nicolas <nicolas.oliva1@mail.udp.cl>
2021-07-10 16:14:00 -0400

    asw

--- servicios/mongotest.py
+++ servicios/mongotest.py
@@ -16,6 +16,15 @@ def post_resena():
     # coleccion.insert_one({"usuario": "Prueba", "password": "prueba123"})
     return resena
 
+def post_coleccion():
+    from pymongo import MongoClient
+    MONGO_URI = "mongodb://uwvux4q3nizdduh0anv5:RNGokX3ElBEv7xE0AYkB@brsadfxyffl9s2t-mongodb.services.clever-cloud.com:27017/brsadfxyffl9s2t"
+    cliente = MongoClient(MONGO_URI)
+    db = cliente["brsadfxyffl9s2t"]
+    colecciones = db["colecciones"]
+    # coleccion.insert_one({"usuario": "Prueba", "password": "prueba123"})
+    return colecciones
+
 # # This is added so that many files can reuse the function get_database()
 if __name__ == "__main__":    
     
