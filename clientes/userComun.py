65975c9925c402b4de7fe3d99b541878503e11c9
Nicolas <nicolas.oliva1@mail.udp.cl>
2021-07-11 17:53:41 -0400

    asdw

--- clientes/userComun.py
+++ clientes/userComun.py
@@ -118,14 +118,14 @@ def handleFiveOption(username):
 def showMenuScreen():
     while True:
         menu = """    
-        □□□□□□□□□□□□□□□□□ Usuario Común □□□□□□□□□□□□□□□□□
-        □ 1.- Realizar una reseña                       □
-        □ 2.- Obtener reseña                            □
-        □ 3.- Agregar juego a la colección              □
-        □ 4.- Consultar Colección                       □
-        □ 5.- Eliminar juego de la colección            □
-        □ 6.- Salir                                     □
-        □□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□"""
+□□□□□□□□□□□□□□□□□ Usuario Común □□□□□□□□□□□□□□□□□
+□ 1.- Realizar una reseña                       □
+□ 2.- Obtener reseña                            □
+□ 3.- Agregar juego a la colección              □
+□ 4.- Consultar Colección                       □
+□ 5.- Eliminar juego de la colección            □
+□ 6.- Salir                                     □
+□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□□"""
         print(menu)
         select = input("Opción: ")
         if(select.isnumeric() and int(select)<=6):
