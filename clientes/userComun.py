8151f425dc5e8382e853a9795a35de277f631d80
Nicolas <nicolas.oliva1@mail.udp.cl>
2021-07-12 14:00:16 -0400

    asdw

--- clientes/userComun.py
+++ clientes/userComun.py
@@ -159,8 +159,8 @@ def handleSixOption():
     table.field_names = ["Nombre", "Publisher","Desarroladora", "Plataforma", "Genero"]
     mensaje = mensaje.split('--')
     print(mensaje)
-    # table.add_row([ mensaje[0], mensaje[1] , mensaje[2], mensaje[3], mensaje[4] ])
-    # print(table)
+    table.add_row([ mensaje[0], mensaje[1] , mensaje[2], mensaje[3], mensaje[4] ])
+    print(table)
     
     print(mensaje)
     input("presione [Enter] para continuar...")
